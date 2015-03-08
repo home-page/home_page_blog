14e7ed7979e1783ddb5437b09f92e7019b7d5763
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-08 22:20:26 +0100

    Set assets precompile paths.

--- lib/home_page_blog/engine.rb
+++ lib/home_page_blog/engine.rb
@@ -3,5 +3,11 @@ module HomePageBlog
     config.autoload_paths << File.expand_path("../../../app/models/concerns", __FILE__)
     config.autoload_paths << File.expand_path("../../../app/controllers/concerns", __FILE__)
     config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
+    
+    config.to_prepare do
+      Rails.application.config.assets.precompile += %w(
+        home_page_blog/*
+      )
+    end 
   end
 end
